ed6a3fd80088ed9df5a27f4aab2fde8db475aeb8
Paugb124 <78257330+Paugb124@users.noreply.github.com>
2024-08-28 19:11:22 +0200

    again

--- TFM/dags/orchestrator.py
+++ TFM/dags/orchestrator.py
@@ -1,43 +1,49 @@
 from airflow import DAG
-from airflow.providers.google.cloud.operators.dataflow import DataflowCreatePythonJobOperator
+from airflow.providers.google.cloud.operators.dataflow import DataflowStartFlexTemplateOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
-# Set env variables
+# Load environment variables
 load_dotenv()
 bucket = os.getenv("BUCKET")
 region = os.getenv("REGION")
 project = os.getenv("PROJECT_ID")
-credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS") # Existe flag para service account en airlfow
-abs_path_pipeline = '/Users/paugarciabardisa/projects/imperia_tfm/TFM/pipeline.py'
+service_account = os.getenv("SERVICE_ACCOUNT_EMAIL", "default-service-account@your-project.iam.gserviceaccount.com")
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': days_ago(1),
     'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-    'run_beam_pipeline',
+    'run_flex_template_pipeline',
     default_args=default_args,
     schedule_interval=timedelta(days=1),
     catchup=False,
     tags=['tfm'],
 ) as dag:
 
-    run_beam = DataflowCreatePythonJobOperator(
-        task_id="run_beam_pipeline",
-        py_file=f'{abs_path_pipeline}',
-        job_name="run_beam_pipeline",
-        options={
-            'project': f'{project}',
-            'region': f'{region}',
-            'temp_location': f'gs://{bucket}/tmp',
-            'staging_location': f'gs://{bucket}/staging',
-            'save_main_session': True,
-            'serviceAccountEmail': 'beam-719@pakotinaikos.iam.gserviceaccount.com'
+    run_flex_template = DataflowStartFlexTemplateOperator(
+        task_id="run_flex_template_pipeline",
+        body={
+            'launchParameter': {
+                'jobName': 'flex-template-job',
+                'containerSpecGcsPath': f'gs://{bucket}/flex_templates/template_spec.json',
+                'parameters': {
+                    'inputFile': f'gs://{bucket}/historico_ventas.csv',
+                    'outputTable': f'{project}:tfm_dataset.historico_ventas',
+                },
+                'environment': {
+                    'serviceAccountEmail': service_account,
+                    'tempLocation': f'gs://{bucket}/tmp'
+                }
+            }
         },
-        py_interpreter="python3",
+        location=region,
+        project_id=project
     )
